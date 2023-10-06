d19598d581b03b6f9797e9b0b68769f114268fa2
Mahmud Hasan <mahmudhassan11752@gmail.com>
2023-10-06 16:39:14 +0600

    responsive issue solved

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,19 +1,25 @@
+import MyTabsComponent from "../Tabs/MyTabsComponent";
+
 const Banner = () => {
   return (
-    <div className="rounded-b-3xl"
+    <div className="rounded-b-3xl h-[600px]"
       style={{
         backgroundImage: 'url("/public/bg-image.jpg")',
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
+      <div className="mx-auto hero-content text-center text-neutral-content">
+        <div className="">
           <h1 className="mb-8 text-2xl mt-24 font-bold">
             Discover the most engaging places
           </h1>
-          <button className="btn bg-[#ff5403] mb-24">Discover on 3D Globe</button>
+          <button className="btn bg-[#ff5403] mb-10 md:mb-24">Discover on 3D Globe</button>
+          <div className="mb-24 px-5 md:px-36 ">
+          <MyTabsComponent></MyTabsComponent>
+          </div>
         </div>
+        
       </div>
     </div>
   );
--- src/Pages/Home/Destination/Destination.jsx
+++ src/Pages/Home/Destination/Destination.jsx
@@ -1,41 +1,46 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import "./Destination.css"
-
+import "./Destination.css";
 
 const Destination = () => {
-    const [destination, setDestination] = useState([])
-    console.log(destination)
-    useEffect(() => {
-        fetch("../../../public/destination.json")
-          .then((res) => res.json())
-          .then((data) => setDestination(data));
-      }, []);
-    return (
-        <div className="max-w-screen-lg mx-auto mt-12 mb-12">
-            <h1 className="text-2xl mb-8">Popular Destination</h1>
-            <div className="md:flex lg:flex md:gap-3 lg:gap-3">
-            {
-                destination.slice(0, 6).map((destDatas, index)=>(<>
-                <div
-                key={index}
-                 >
-  <div className="mybox">
-  <figure><img className="rounded-3xl h-[180px] w-[200px] desImage" src={destDatas.image} alt="Shoes" /></figure>
-  <div className="mycontent">
-  <a className="btn bg-[#ff5403] btn-sm" href="">See More</a>
-  </div>
-  </div>
-  <div >
-    <h2 >{destDatas.name}</h2>
-    <p>{destDatas.place}</p>
-  </div>
-</div>
-                </>))
-            }
+  const [destination, setDestination] = useState([]);
+  console.log(destination);
+  useEffect(() => {
+    fetch("../../../public/destination.json")
+      .then((res) => res.json())
+      .then((data) => setDestination(data));
+  }, []);
+  return (
+    <div className="max-w-screen-lg mx-auto mt-12 mb-12">
+      <h1 className="text-2xl mb-8 text-center">Popular Destination</h1>
+      <div className="grid grid-cols-1 md:grid-cols-6 md:gap-3 lg:gap-3 justify-center items-center">
+        {destination.slice(0, 6).map((destDatas, index) => (
+          <>
+            <div className="mx-auto" key={index}>
+              <div className="mybox">
+                <figure>
+                  <img
+                    className="rounded-3xl h-[180px] w-[200px] desImage"
+                    src={destDatas.image}
+                    alt="Shoes"
+                  />
+                </figure>
+                <div className="mycontent">
+                  <a className="btn bg-[#ff5403] btn-sm" href="">
+                    See More
+                  </a>
+                </div>
+              </div>
+              <div>
+                <h2>{destDatas.name}</h2>
+                <p>{destDatas.place}</p>
+              </div>
             </div>
-        </div>
-    );
+          </>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Destination;
