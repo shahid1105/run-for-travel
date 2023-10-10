b72293ccfe3d3e5f5f197d4a4965bd0417e2aea9
shahid1105 <shahidhasan1105@gmail.com>
2023-10-10 23:21:18 +0600

    some changes

--- src/Pages/Home/Destination/Destination.jsx
+++ src/Pages/Home/Destination/Destination.jsx
@@ -13,15 +13,15 @@ const Destination = () => {
       }, []);
     return (
         <div className="max-w-screen-lg mx-auto mt-12 mb-12">
-            <h1 className="text-2xl mb-8">Popular Destination</h1>
-            <div className="md:flex lg:flex md:gap-3 lg:gap-3">
+            <h1 className="text-2xl mb-8 ps-4">Popular Destination</h1>
+            <div className="md:flex lg:flex md:gap-3 lg:gap-3 px-4">
             {
                 destination.slice(0, 6).map((destDatas, index)=>(<>
                 <div
                 key={index}
                  >
   <div className="mybox">
-  <figure><img className="rounded-3xl h-[180px] w-[200px] desImage" src={destDatas.image} alt="Shoes" /></figure>
+  <figure><img className="rounded-3xl md:h-[160px] md:w-[180px] lg:h-[180px] lg:w-[200px]" src={destDatas.image} alt="Shoes" /></figure>
   <div className="mycontent">
   <a className="btn bg-[#ff5403] btn-sm" href="">See More</a>
   </div>
